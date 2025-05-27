8eedb42aad9dc671fd1e01c24dd86b422e944768
vladsolo <solo.vlad55555@icloud.com>
2025-05-27 15:18:26 +0300

    хз

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import asyncio
+from aiogram import Bot, Dispatcher
+from dotenv import find_dotenv, load_dotenv
+from loguru import logger
+from handlers.user_private import user_private_router
+from handlers.group_chat import group_router
+from utils.scheduler import schedule_jobs
+from database.db import init_db
+
+load_dotenv(find_dotenv())
+TOKEN = os.getenv("TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
+if not TOKEN or not CHANNEL_ID:
+    raise ValueError("Не удалось загрузить переменные из .env")
+
+logger.add("bot.log", rotation="10 MB", level="INFO")
+logger.info("Бот запущен")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+dp.include_router(user_private_router)
+dp.include_router(group_router)
+
+async def on_startup():
+    await init_db()
+    asyncio.create_task(schedule_jobs(bot))
+
+async def main():
+    await on_startup()
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
